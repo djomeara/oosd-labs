a5e033e126e028c3e567a8e97c43be94dc26f105
dylan <dylan@10.40.11.87>
2017-09-26 10:27:50 +0100

    newer

--- OOSD/src/ie/LAB3/oosd/LAB3B.java
+++ OOSD/src/ie/LAB3/oosd/LAB3B.java
@@ -6,10 +6,21 @@ public class LAB3B {
 	{
 		Scanner input = new Scanner( System.in );
 		String string1;
-		String string2;
+		String string2 = "";
+		char char1;
+		int index;
 		
+		System.out.print("Enter your word : ");
 		string1 = input.next();
-		string2 = input.next();
+		System.out.println("");
+		
+		for (index=string1.length()-1; index>=0; index--)
+			{
+				char1 = string1.charAt(index);
+				string2 +=char1;
+			}
+		System.out.println("The Reverse is : " +string2);
+			
 	}
 
 }
