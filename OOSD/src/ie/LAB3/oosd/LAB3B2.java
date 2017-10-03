eed5daae7c1432634feaa55d16b4890d069673da
dylan <dylan@10.40.14.250>
2017-10-03 09:29:47 +0100

    SUPER IMPORTANT

--- OOSD/src/ie/LAB3/oosd/LAB3B2.java
+++ OOSD/src/ie/LAB3/oosd/LAB3B2.java
@@ -1,10 +1,38 @@
 package ie.LAB3.oosd;
-
+import java.util.Scanner;
 public class LAB3B2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner ( System.in);
+		String sent1;
+		String sent2 = "";
+		int value;
+		int ascii;
+		int index;
+		char currentChar;
+	
+		System.out.print("Enter your word : ");
+		sent1 = input.next();
+		System.out.println("");
+		for(index=0; index<sent1.length();index++)
+			{
+				value = sent1.charAt(index);
+				ascii = (int)value;
+				currentChar=(char) ( ascii+1);
+				sent2 += currentChar;
+			}
+		System.out.println(sent2);
 
 	}
 
 }
+
+/*	value = sent1.charAt(index);
+	ascii = (int)value;
+	currentChar=(char) ( ascii+1);
+	sent2 += currentChar;
+	
+	is needed to cast a char to ascii and back to char  
+	
+	*/
