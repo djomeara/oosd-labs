83b3ab61cebb2edb7a4acd92ac47995fc41ac98e
dylan <dylan@10.40.14.250>
2017-10-03 10:15:25 +0100

    New Object

---
+++ OOSD/src/ie/THERM/oosd/Thermometer.java
@@ -0,0 +1,20 @@
+package ie.THERM.oosd;
+
+public class Thermometer 
+{
+	private double celsius;
+
+	public Thermometer(double cels) 
+	{
+		celsius = cels;
+	}
+
+	public double getCelsius() {
+		return celsius;
+	}
+
+	public void setCelsius(double cels) {
+		celsius = cels;
+	}
+	
+}
